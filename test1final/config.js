4a0d7a8205e84eca5132c118de74145a27b2941b
izzuddinzuhri <45057644+izzuddinzuhri@users.noreply.github.com>
2019-08-24 01:27:08 +0800

    Sign in folder

---
+++ test1final/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCj_txSeHQJpxd9Wt0fAVzc3QE-RQ03oC8",
+    authDomain: "react-native-1c0f0.firebaseapp.com",
+    databaseURL: "https://react-native-1c0f0.firebaseio.com",
+    projectId: "react-native-1c0f0",
+    storageBucket: "",
+    messagingSenderId: "893940371680",
+    appId: "1:893940371680:web:7acec0ace772e28a"
+  };
